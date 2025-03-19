55c2e45eb3b81f21e8b8efbfda68b2af112f0cfb
Constantin Philippenko <cphilippenko@free.fr>
2025-03-19 11:55:21 +0100

    Add PlotUtilities.

---
+++ src/utils/PlotUtilities.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.data.DatasetConstants import BATCH_SIZE, STEP_SIZE
+from src.utils.Utilities import get_project_root, create_folder_if_not_existing
+
+COLORS = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+
+def plot_values(epochs, values, legends, metric_name, dataset_name, log=False):
+    """
+    Plots the given values against epochs using 'tab:colors'.
+
+    Args:
+        epochs (list or array): List of epoch numbers.
+        values (list or array): List of correspplt.plot(epochs[algo_name][0], np.mean(values[algo_name], axis=0), marker='o', linestyle='-', color=COLORS[i], label=algo_name)
+        onding values.
+        name (str): Label for the y-axis.
+    """
+    plt.figure(figsize=(8, 5))
+    i = 0
+    for algo_name in legends:
+        if log:
+            avg_values = np.mean([np.log10(v) for v in values[algo_name]], axis=0)
+            avg_values_var = np.std([np.log10(v) for v in values[algo_name]], axis=0)
+            plt.plot(epochs[algo_name][0], avg_values, marker='o', linestyle='-', color=COLORS[i], label=algo_name)
+            plt.fill_between(epochs[algo_name][0], avg_values - avg_values_var, avg_values + avg_values_var, alpha=0.2,
+                             color=COLORS[i])
+
+        else:
+            avg_values = np.mean(values[algo_name], axis=0)
+            avg_values_var = np.std(values[algo_name], axis=0)
+            plt.plot(epochs[algo_name][0], avg_values, marker='o', linestyle='-', color=COLORS[i], label=algo_name)
+            plt.fill_between(epochs[algo_name][0], avg_values - avg_values_var, avg_values + avg_values_var, alpha=0.2,
+                             color=COLORS[i])
+
+        i+=1
+    plt.ylabel(metric_name)
+    plt.legend()
+
+    root = get_project_root()
+    folder = f'{root}/pictures/convergence/{dataset_name}'
+    create_folder_if_not_existing(folder)
+    plt.savefig(f"{folder}/{metric_name}_b{BATCH_SIZE[dataset_name]}.pdf", bbox_inches='tight', dpi=600)
